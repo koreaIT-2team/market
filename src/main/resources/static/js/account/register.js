99eaf0db6d08d0827d774b63a11ec5616bb0177b
gahyeon noh <yes-7r@naver.com>
2022-11-08 14:04:09 +0900

    발표 ppt / pdf

--- src/main/resources/static/js/account/register.js
+++ src/main/resources/static/js/account/register.js
@@ -12,16 +12,16 @@ registerButton.onclick = () => {
 
 
     $.ajax({
-            async: false,                       //필수
-            type: "post",                       //필수
-            url: "/api/account/register",       //필수
-            contentType: "application/json",    //전송할 데이터가 json인 경우
-            data: JSON.stringify(user),         //전송할 데이터가 있으면
-            dataType: "json",                   //json외 text 등을 사용할 수 있지만 json 사용함
+            async: false,
+            type: "post",
+            url: "/api/account/register",
+            contentType: "application/json",
+            data: JSON.stringify(user),
+            dataType: "json",
             success: (response) => {
-                    console.log(response);      //성공시에 실행될 메소드
+                    console.log(response);
             },
-            error: (error) => {                 //실패시에 실행될 메소드
+            error: (error) => {
                 console.log(error.responseJSON.data);
                 loadErrorMessage(error.responseJSON.data);
             }
